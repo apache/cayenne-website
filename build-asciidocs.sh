d849147af6987044b95ab4aeb533c038a703a1d7
Nikita Timofeev <stariy95@gmail.com>
2018-01-08 16:45:25 +0300

    Add script for fast asciidoc build

--- build-asciidocs.sh
+++ build-asciidocs.sh
@@ -18,14 +18,13 @@
 # under the License.
 #
 
+
 #
-# This shell script can be used to add or update cayenne documentation.
-# It can update HTML guides and JavaDoc for one version at a time.
-# Should be used from default project location.
+# This shell script can be used to fast add or update cayenne Asciidoc documentation.
 # For safety reason changes should be committed manually.
 #
 #  Usage:
-#  > build-docs.sh git-tag cayenne-version
+#  > build-asciidocs.sh git-tag cayenne-version
 #
 #  Options:
 #       git-tag: tag that will be used to build documentation
@@ -34,10 +33,13 @@
 #
 #  Example:
 #       Build docs for release 4.1.M1:
-#       > build-docs.sh 4.1.M1
+#       > build-asciidocs.sh 4.1
 #
 #       Build docs for old 3.1.1 release:
-#       > build-docs.sh cayenne-parent-3.1.1 3.1.1
+#       > build-asciidocs.sh cayenne-parent-3.1.1 3.1
+#
+#       Build docs for master:
+#       > build-asciidocs.sh master 4.1
 #
 
 function checkAndCreateDir() {
@@ -56,7 +58,7 @@ function clearDir() {
 
 # no input, so just exit
 if [ -z "$1" ]; then
-    echo "Usage: build-docs.sh git-tag [cayenne-version]"
+    echo "Usage: build-asciidocs.sh git-tag [cayenne-version]"
     exit -1
 fi
 
@@ -78,34 +80,28 @@ echo "Working dir: $BASE_DIR"
 
 # init and check paths
 MAJOR_VERSION="${VERSION:0:3}" # expecting version format as X.Y.something-else, i.e. 4.1.M1 is ok, but 4.12.M1 will fail..
-JAVA_DOC_DIR="$BASE_DIR/src/main/site/static/docs/$MAJOR_VERSION"       # JavaDoc goes to static, no template wrapping
 ASCII_DOC_DIR="$BASE_DIR/src/main/site/content/docs/$MAJOR_VERSION"     # Asciidoc goes to content, Hugo will process it
 CAYENNE_TMP_DIR="$BASE_DIR/target/cayenne-tmp"                          # tmp directory to checkout Cayenne
 
 # prepare all directories
-#clearDir          "$ASCII_DOC_DIR"
 clearDir          "$CAYENNE_TMP_DIR"
 checkAndCreateDir "$ASCII_DOC_DIR"
-checkAndCreateDir "$JAVA_DOC_DIR"
 
 echo "Building docs for Cayenne $MAJOR_VERSION ($VERSION)"
 
 # clone git repo and checkout requested TAG
 git clone https://github.com/apache/cayenne.git "$CAYENNE_TMP_DIR" --branch "$GIT_TAG" --depth 1
-cd  "$CAYENNE_TMP_DIR"
+# we will need Maven to build only asciidoc modules
+cd  "$CAYENNE_TMP_DIR/docs/asciidoc/"
 
 # build it
 echo "Running Maven build... it can take a while..."
 mvn install -q -DskipTests > /dev/null 2>&1
 echo "Maven build complete"
 
-# copy JavaDoc
-echo "Syncing JavaDoc to \"docs/$MAJOR_VERSION/api/\""
-rsync -a --delete "./docs/doc/target/site/apidocs/doc/api/" "$JAVA_DOC_DIR/api/"
-
 # copy everything from ./docs/asciidoc/**/target/site/** directories
-cd "$CAYENNE_TMP_DIR/docs/asciidoc/"
 for d in */ ; do
+    # skip asciidoc extension module
     if [ "$d" == "cayenne-asciidoc-extension/" ]; then
         continue
     fi
@@ -113,7 +109,3 @@ for d in */ ; do
     echo "Syncing asciidoc content for ${d}"
     cp -R "./${d}target/site/." "$ASCII_DOC_DIR/"
 done
-
-# Commit changes, disabled for now
-# cd "$BASE_DIR"
-# git add -A && git commit -m "[build-docs] Update documentation for Cayenne $MAJOR_VERSION"
